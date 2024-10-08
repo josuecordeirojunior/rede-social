daa15e324a829a76754116248624fbeb23531f3a
josuecordeirojunior <105441901+josuecordeirojunior@users.noreply.github.com>
2024-10-08 09:00:38 -0300

    Add files via upload

---
+++ kauã 3c/graficos/quantidadeUsuarios.js
@@ -0,0 +1,21 @@
+async function quantidadeUsuariosPorRede() {
+    const res = await fetch(url)
+    const dados = await res.json()
+    const nomeDasRedes = Object.keys(dados)
+    const quantidadeDeUsuarios = Object.values(dados)
+
+    const data = [
+        {
+            x: nomeDasRedes, 
+            y: quantidadeDeUsuarios, 
+            type: 'bar'
+        }
+    ]
+
+    const grafico = document.createElement('div')
+    grafico.className = 'grafico'
+    document.getElementById('graficos-container').appendChild(grafico)
+    Plotly.newPlot(grafico, data)
+}
+
+quantidadeUsuariosPorRede()
